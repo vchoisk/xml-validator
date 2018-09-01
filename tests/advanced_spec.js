7b3de94c0a49b2ea03679dea1f4cc6d1c5594833
vchoisk <vchoisk@gmail.com>
2018-09-01 12:15:21 +0900

    seperated invalid input type testing

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -13,7 +13,7 @@ describe("advanced validator test", () => {
     ).toBeTruthy();
   });
 
-  it("should return true for an xml with none consecutive node with same tag", () => {
+  it("should return true for an xml with non-consecutive node with same tag", () => {
     expect(isValidXML("<a/><b/><a/><b/>")).toBeTruthy();
   });
 
@@ -25,16 +25,40 @@ describe("advanced validator test", () => {
     expect(isValidXML("dsaf{{L<PL<KLLL@!#12")).toBeFalsy();
   });
 
-  it("should return false for non-string input", () => {
+  it("should return false for no input", () => {
     expect(isValidXML()).toBeFalsy();
+  });
+
+  it("should return false for integer input", () => {
     expect(isValidXML(123)).toBeFalsy();
+  });
+
+  it("should return false for float input", () => {
     expect(isValidXML(123.1)).toBeFalsy();
+  });
+
+  it("should return false for null input", () => {
     expect(isValidXML(null)).toBeFalsy();
+  });
+
+  it("should return false for undefined input", () => {
     expect(isValidXML(undefined)).toBeFalsy();
+  });
+
+  it("should return false for boolean input", () => {
     expect(isValidXML(true)).toBeFalsy();
     expect(isValidXML(false)).toBeFalsy();
+  });
+
+  it("should return false for object input", () => {
     expect(isValidXML({})).toBeFalsy();
+  });
+
+  it("should return false for array input", () => {
     expect(isValidXML([])).toBeFalsy();
+    expect(isValidXML([1, 3, 3])).toBeFalsy();
+    expect(isValidXML(["a", "b", "c"])).toBeFalsy();
+    expect(isValidXML(["<a/>", "<b/>", "<c/>"])).toBeFalsy();
   });
 
   it("should return false for an xml with child nodes, one of which has same tag as its parent", () => {
