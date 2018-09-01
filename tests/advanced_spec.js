97f982010c846a1f4c42e2a83d906093be0e76df
vchoisk <vchoisk@gmail.com>
2018-09-01 11:49:50 +0900

    fixed typo on should

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -3,17 +3,17 @@
 const { isValidXML } = require("../src");
 
 describe("advanced validator test", () => {
-  it("hould return true for an xml with multiple top level nodes, each with a child node different ", () => {
+  it("should return true for an xml with multiple top level nodes, each with a child node different ", () => {
     expect(isValidXML("<a><d/></a><b><d/></b><c><d/></c>")).toBeTruthy();
   });
 
-  it("hould return true for an xml with multiple top level nodes, each with child nodes with unique tags ", () => {
+  it("should return true for an xml with multiple top level nodes, each with child nodes with unique tags ", () => {
     expect(
       isValidXML("<a><d/><e/></a><b><e/><d/></b><c><d/><e/></c>")
     ).toBeTruthy();
   });
 
-  it("hould return false for an xml with child nodes, one of which has same tag as its parent ", () => {
+  it("should return false for an xml with child nodes, one of which has same tag as its parent ", () => {
     expect(isValidXML("<a><d/><a/></a>")).toBeFalsy();
   });
 });
