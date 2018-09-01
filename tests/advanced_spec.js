735a43fc5d7bd803b9980edf0614064f4a2a5518
vchoisk <vchoisk@gmail.com>
2018-09-01 11:45:30 +0900

    add advenced tests/ case multiple top level nodes that has a child/ case multiple top level node with multiple child nodes

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -3,7 +3,12 @@
 const { isValidXML } = require("../src");
 
 describe("advanced validator test", () => {
-  it("FILL ME", () => {
-    // FILL ME
+  it("hould return true for an xml with multiple top level nodes, each with a child node different ", () => {
+    expect(isValidXML("<a><d/></a><b><d/></b><c><d/></c>")).toBeTruthy();
+  });
+  it("hould return true for an xml with multiple top level nodes, each with child nodes with unique tags ", () => {
+    expect(
+      isValidXML("<a><d/><e/></a><b><e/><d/></b><c><d/><e/></c>")
+    ).toBeTruthy();
   });
 });
